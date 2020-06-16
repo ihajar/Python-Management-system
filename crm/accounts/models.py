49055786de97a2bc3a783dea61aef66b3e841284
ihajar <hadjero.hamaidi@gmail.com>
2020-06-16 16:08:28 +0100

    Database management

--- crm/accounts/models.py
+++ crm/accounts/models.py
@@ -10,6 +10,12 @@ class Customer(models.Model):
     def __str__(self):
         return self.name
 
+class Tag(models.Model):
+    name = models.CharField(max_length=200, null=True)
+  
+    def __str__(self):
+        return self.name
+
 
 class Product(models.Model):
     CATEGORY = (
@@ -19,8 +25,14 @@ class Product(models.Model):
     name = models.CharField(max_length=200, null=True)
     price = models.FloatField(null=True)
     category = models.CharField(max_length=200, null=True, choices=CATEGORY)
-    description = models.CharField(max_length=200, null=True)
+    description = models.CharField(max_length=200, null=True, blank=True)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
+    tags = models.ManyToManyField(Tag)
+    
+    
+    def __str__(self):
+        return self.name
+
 
 class Order(models.Model):
 
@@ -29,7 +41,7 @@ class Order(models.Model):
         ('Out of delivery', 'Out of delivery'),
         ('Delivered', 'Delivered')
     )
-    #customer  = 
-    #product = 
+    customer  = models.ForeignKey(Customer, null=True, on_delete=models.SET_NULL)
+    product = models.ForeignKey(Product, null=True, on_delete=models.SET_NULL)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
     status = models.CharField(max_length=200, null=True, choices=STATUS)
